4cea2a38f8357496391ffaae9f10f9d4d8ee78c5
Raven Lagrimas <rjlagrimas08@gmail.com>
2015-01-21 10:35:50 +0800

    Refactored code

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 var mongo = require('mongoskin'),
-    db_config;
 
-module.exports = function(_db_config) {
-    db_config = db_config || _db_config;
+    cached_config,
 
-    return mongo.db('mongodb://'
-            + db_config.host
-            + ':'
-            + db_config.port
-            + '/'
-            + db_config.name,
-        {native_parser : true}
-    );
-};
+    mongo_wrapper = function(config) {
+        cached_config = config || cached_config;
+
+        return mongo.db('mongodb://'
+                + cached_config.host
+                + ':'
+                + cached_config.port
+                + '/'
+                + cached_config.name,
+            {native_parser : true}
+        );
+    };
+
+
+module.exports = mongo_wrapper;
